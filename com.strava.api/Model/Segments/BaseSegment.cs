912f1bff90f3f79468161b11bd8552974f9ed52f
cgourlay <colin.gourlay5@gmail.com>
2015-06-11 13:48:01 +0100

    Added latitude/longitude support in the BaseSegment

--- com.strava.api/Model/Segments/BaseSegment.cs
+++ com.strava.api/Model/Segments/BaseSegment.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Device.Location;
+using Newtonsoft.Json;
 
 namespace com.strava.api.Model.Segments
 {
@@ -8,22 +9,25 @@ namespace com.strava.api.Model.Segments
         public int Id { get; set; }
 
         [JsonProperty("activity_type")]
-        public string ActivityType { get; set; } // TODO: CG to complete... This can be ‘Ride’ or ‘Run’
+        public string ActivityType { get; set; } // TODO: CG to complete... This can be ‘Ride’ or ‘Run’ - do we really want to use a string?
 
         [JsonProperty("average_grade")]
         public float AverageGrade { get; set; }
 
         [JsonProperty("climb_category")]
-        public int Category { get; set; } // TODO: CG to complete... This is a number in the range 0 to 5 where 0 is lower and 5 is harder.
+        public int ClimbCategory { get; set; } // TODO: CG to complete... This is a number in the range 0 to 5 where 0 is lower and 5 is harder - do we really want to use an int?
 
         [JsonProperty("city")]
         public string City { get; set; }
 
         [JsonProperty("country")]
-        public string Country { get; set; } // TODO: CG to complete... Do we really want a string here?
+        public string Country { get; set; } // TODO: CG to complete... Do we really want to use a string here?
 
         [JsonProperty("distance")]
         public float Distance { get; set; }
+
+        [JsonProperty("end_latlng")]
+        public GeoCoordinate EndCoordinates { get; set; }
 
         [JsonProperty("private")]
         public bool IsPrivate { get; set; }
@@ -46,11 +50,10 @@ namespace com.strava.api.Model.Segments
         [JsonProperty("starred")]
         public bool IsStarred { get; set; }
 
+        [JsonProperty("start_latlng")]
+        public GeoCoordinate StartCoordinates { get; set; }
+
         [JsonProperty("state")]
         public string State { get; set; }
-
-        // TODO: CG to complete... The base segment should capture the start and end latitude/longitude.
-        // start_latlng: [latitude, longitude]
-        // end_latlng:	[latitude, longitude]
     }
 }
