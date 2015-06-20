e033a11d6e6f12014bc28041b8e870bcb387508a
cgourlay <colin.gourlay5@gmail.com>
2015-06-20 15:47:51 +0100

    Issue #4 - Added tests and included them in the build

--- com.strava.api/Model.Tests/ClassLibrary1/Segments/SegmentTests.cs
+++ com.strava.api/Model.Tests/ClassLibrary1/Segments/SegmentTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using com.strava.api.Model.Segments;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,27 @@ namespace Model.Tests.Segments
     [TestFixture]
     public class SegmentTests
     {
-        [Test]
-        public void SomeTest()
+        public class Created : SegmentTests
         {
-            Assert.That(true == true);
+            [Test]
+            public void CanGetCreationDateTime()
+            {
+                var someDateTime = DateTime.Now;
+                var segment = new Segment() { Created = someDateTime };
+
+                Assert.That(segment.Created, Is.EqualTo(someDateTime));
+            }
+
+            [Test]
+            public void CanSetCreationDate()
+            {
+                var someDateTime = DateTime.Now;
+                var segment = new Segment();
+
+                Assert.That(segment.Created, Is.EqualTo(DateTime.MinValue));
+                segment.Created = someDateTime;
+                Assert.That(segment.Created, Is.EqualTo(someDateTime));
+            }
         }
     }
 }
