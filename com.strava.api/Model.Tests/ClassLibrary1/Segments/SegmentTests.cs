9b2e53e747b8fa8050e94b1d84479ff09e24a982
cgourlay <colin.gourlay5@gmail.com>
2015-06-21 19:21:33 +0100

    Enhanced code coverage of the (de)serialization of Segment

--- com.strava.api/Model.Tests/ClassLibrary1/Segments/SegmentTests.cs
+++ com.strava.api/Model.Tests/ClassLibrary1/Segments/SegmentTests.cs
@@ -17,14 +17,30 @@ namespace Model.Tests.Segments
         public class Created : SegmentTests
         {
             [Test]
-            public void CanGetDateTimeCreated()
+            public void CanDeserialiseFromJson()
+            {
+                string json = "{ \"created_at\":\"2015-06-21T19:08:00\" }";
+                var segment = JsonConvert.DeserializeObject<Segment>(json);
+                Assert.That(segment.Created, Is.EqualTo(new DateTime(2015, 6, 21, 19, 8, 0)));
+            }
+
+            [Test]
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.Created, Is.EqualTo(DateTime.MinValue));
             }
 
             [Test]
-            public void CanSetDateTimeCreated()
+            public void CanSerialiseToJson()
+            {
+                var segment = new Segment() { Created = new DateTime(2015, 6, 21, 19, 8, 0) };
+                var output = JsonConvert.SerializeObject(segment);
+                Assert.That(output, Text.Contains("\"created_at\":\"2015-06-21T19:08:00\""));
+            }
+
+            [Test]
+            public void CanSet()
             {
                 var someDateTime = DateTime.Now;
                 var segment = new Segment();
@@ -37,7 +53,7 @@ namespace Model.Tests.Segments
         public class IsHazardous : SegmentTests
         {
             [Test]
-            public void CanDeserialiseJsonAndSetIsHazardousToFalse()
+            public void CanDeserialiseFromJson()
             {
                 string json = "{ \"hazardous\": false }";
                 var segment = JsonConvert.DeserializeObject<Segment>(json);
@@ -45,31 +61,14 @@ namespace Model.Tests.Segments
             }
 
             [Test]
-            public void CanDeserialiseJsonAndSetIsHazardousToTrue()
-            {
-                string json = "{ \"hazardous\": true }";
-                var segment = JsonConvert.DeserializeObject<Segment>(json);
-                Assert.That(segment.IsHazardous, Is.True);
-            }
-
-            [Test]
-            public void CanGetIsHazardous()
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.IsHazardous, Is.False);
             }
 
             [Test]
-            public void CanSetIsHazardous()
-            {
-                var segment = new Segment();
-
-                segment.IsHazardous = true;
-                Assert.That(segment.IsHazardous, Is.True);
-            }
-
-            [Test]
-            public void CanSerialiseToJsonWhenIsHazardousIsFalse()
+            public void CanSerialiseToJson()
             {
                 var segment = new Segment() { IsHazardous = false };
                 var output = JsonConvert.SerializeObject(segment);
@@ -77,25 +76,26 @@ namespace Model.Tests.Segments
             }
 
             [Test]
-            public void CanSerialiseToJsonWhenIsHazardousIsTrue()
+            public void CanSet()
             {
-                var segment = new Segment() { IsHazardous = true };
-                var output = JsonConvert.SerializeObject(segment);
-                Assert.That(output, Text.Contains("\"hazardous\":true"));
+                var segment = new Segment();
+
+                segment.IsHazardous = true;
+                Assert.That(segment.IsHazardous, Is.True);
             }
         }
 
         public class Map : SegmentTests
         {
             [Test]
-            public void CanGetMap()
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.Map, Is.Null);
             }
 
             [Test]
-            public void CanSetMap()
+            public void CanSet()
             {
                 var segment = new Segment();
                 var mock = new Mock<IMap>().Object;
@@ -108,14 +108,30 @@ namespace Model.Tests.Segments
         public class NumberOfAthletes : SegmentTests
         {
             [Test]
-            public void CanGetNumberOfAthletes()
+            public void CanDeserialiseFromJson()
+            {
+                string json = "{ \"athlete_count\": 7036 }";
+                var segment = JsonConvert.DeserializeObject<Segment>(json);
+                Assert.That(segment.NumberOfAthletes, Is.EqualTo(7036));
+            }
+
+            [Test]
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.NumberOfAthletes, Is.EqualTo(0));
             }
 
             [Test]
-            public void CanSetNumberOfAthletes()
+            public void CanSerialiseToJson()
+            {
+                var segment = new Segment() { NumberOfAthletes = 7036 };
+                var output = JsonConvert.SerializeObject(segment);
+                Assert.That(output, Text.Contains("\"athlete_count\":7036"));
+            }
+
+            [Test]
+            public void CanSet()
             {
                 var segment = new Segment();
 
@@ -127,52 +143,100 @@ namespace Model.Tests.Segments
         public class NumberOfEfforts : SegmentTests
         {
             [Test]
-            public void CanGetNumberOfEfforts()
+            public void CanDeserialiseFromJson()
+            {
+                string json = "{ \"effort_count\": 51335 }";
+                var segment = JsonConvert.DeserializeObject<Segment>(json);
+                Assert.That(segment.NumberOfEfforts, Is.EqualTo(51335));
+            }
+
+            [Test]
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.NumberOfEfforts, Is.EqualTo(0));
             }
 
             [Test]
-            public void CanSetNumberOfEfforts()
+            public void CanSerialiseToJson()
+            {
+                var segment = new Segment() { NumberOfEfforts = 51335 };
+                var output = JsonConvert.SerializeObject(segment);
+                Assert.That(output, Text.Contains("\"effort_count\":51335"));
+            }
+
+            [Test]
+            public void CanSet()
             {
                 var segment = new Segment();
 
-                segment.NumberOfEfforts = 100;
-                Assert.That(segment.NumberOfEfforts, Is.EqualTo(100));
+                segment.NumberOfEfforts = 51335;
+                Assert.That(segment.NumberOfEfforts, Is.EqualTo(51335));
             }
         }
 
         public class NumberOfStars : SegmentTests
         {
             [Test]
-            public void CanGetNumberOfStars()
+            public void CanDeserialiseFromJson()
+            {
+                string json = "{ \"star_count\": 3 }";
+                var segment = JsonConvert.DeserializeObject<Segment>(json);
+                Assert.That(segment.NumberOfStars, Is.EqualTo(3));
+            }
+
+            [Test]
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.NumberOfStars, Is.EqualTo(0));
             }
 
             [Test]
-            public void CanSetNumberOfStars()
+            public void CanSerialiseToJson()
+            {
+                var segment = new Segment() { NumberOfStars = 51 };
+                var output = JsonConvert.SerializeObject(segment);
+                Assert.That(output, Text.Contains("\"star_count\":51"));
+            }
+
+            [Test]
+            public void CanSet()
             {
                 var segment = new Segment();
 
-                segment.NumberOfStars = 100;
-                Assert.That(segment.NumberOfStars, Is.EqualTo(100));
+                segment.NumberOfStars = 5;
+                Assert.That(segment.NumberOfStars, Is.EqualTo(5));
             }
         }
 
         public class TotalElevationGain : SegmentTests
         {
             [Test]
-            public void CanGetTotalElevationGain()
+            public void CanDeserialiseFromJson()
+            {
+                string json = "{ \"total_elevation_gain\": 3.465 }";
+                var segment = JsonConvert.DeserializeObject<Segment>(json);
+                Assert.That(segment.TotalElevationGain, Is.EqualTo(3.465f));
+            }
+
+            [Test]
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.TotalElevationGain, Is.EqualTo(0f));
             }
 
             [Test]
-            public void CanSetTotalElevationGain()
+            public void CanSerialiseToJson()
+            {
+                var segment = new Segment() { TotalElevationGain = 155.7f };
+                var output = JsonConvert.SerializeObject(segment);
+                Assert.That(output, Text.Contains("\"total_elevation_gain\":155.7"));
+            }
+
+            [Test]
+            public void CanSet()
             {
                 var segment = new Segment();
 
@@ -184,14 +248,30 @@ namespace Model.Tests.Segments
         public class Updated : SegmentTests
         {
             [Test]
-            public void CanGetDateTimeUpdated()
+            public void CanDeserialiseFromJson()
+            {
+                string json = "{ \"updated_at\":\"2015-06-21T19:08:00\" }";
+                var segment = JsonConvert.DeserializeObject<Segment>(json);
+                Assert.That(segment.Updated, Is.EqualTo(new DateTime(2015, 6, 21, 19, 8, 0)));
+            }
+
+            [Test]
+            public void CanGet()
             {
                 var segment = new Segment();
                 Assert.That(segment.Updated, Is.EqualTo(DateTime.MinValue));
             }
 
             [Test]
-            public void CanSetDateTimeUpdated()
+            public void CanSerialiseToJson()
+            {
+                var segment = new Segment() { Updated = new DateTime(2015, 6, 21, 19, 8, 0) };
+                var output = JsonConvert.SerializeObject(segment);
+                Assert.That(output, Text.Contains("\"updated_at\":\"2015-06-21T19:08:00\""));
+            }
+
+            [Test]
+            public void CanSet()
             {
                 var someDateTime = DateTime.Now;
                 var segment = new Segment();
