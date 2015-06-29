c64ccbfe10cce676eeedaec59ea43e2624e02f8b
cgourlay <colin.gourlay5@gmail.com>
2015-06-29 07:25:39 +0100

    Modified the BaseRepresentation.

--- com.strava.api/Representations.Tests/Representations.Tests/BaseRepresentationTests.cs
+++ com.strava.api/Representations.Tests/Representations.Tests/BaseRepresentationTests.cs
@@ -1,9 +1,46 @@
 ﻿using NUnit.Framework;
 
+using com.strava.api.Representations;
+
 namespace Representations.Tests
 {
     [TestFixture]
     public class BaseRepresentationTests
     {
+        public class ApplicationNamespace : BaseRepresentationTests
+        {
+            [Test]
+            public void CanGet()
+            {
+                Assert.That(BaseRepresentation.ApplicationNamespace, Is.EqualTo(@"http://schemas.swim-bike-run/tavla"));
+            }
+        }
+
+        public class DomainApplicationProtocolNamespace : BaseRepresentationTests
+        {
+            [Test]
+            public void CanGet()
+            {
+                Assert.That(BaseRepresentation.DomainApplicationProtocolNamespace, Is.EqualTo(@"http://schemas.swim-bike-run/tavla/dap"));
+            }
+        }
+
+        public class ApplicationMediaType : BaseRepresentationTests
+        {
+            [Test]
+            public void CanGet()
+            {
+                Assert.That(BaseRepresentation.ApplicationMediaType, Is.EqualTo(@"application/vnd.tavla+json"));
+            }
+        }
+
+        public class SelfRelationValue : BaseRepresentationTests
+        {
+            [Test]
+            public void CanGet()
+            {
+                Assert.That(BaseRepresentation.SelfRelationValue, Is.EqualTo(@"self"));
+            }
+        }
     }
 }
