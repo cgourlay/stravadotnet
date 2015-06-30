763ed0019eed26cb49b94d6d4313c9fe5f4e3f0a
cgourlay <colin.gourlay5@gmail.com>
2015-06-30 07:48:54 +0100

    Changed from a class library to a Nancy self hosted application.

---
+++ com.strava.api/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+
+using Nancy.Hosting.Self;
+
+namespace com.strava.api
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var serviceUri = new Uri(ConfigurationManager.AppSettings["ServiceUri"]);
+            var configuration = new HostConfiguration
+            {
+                AllowChunkedEncoding = false,
+                RewriteLocalhost = false,
+                UrlReservations = new UrlReservations {CreateAutomatically = true}
+            };
+
+            using (var host = new NancyHost(configuration, serviceUri))
+            {
+                host.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
