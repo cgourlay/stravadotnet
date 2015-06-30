dbc24ceca852e333246ca97f1b32cbdb9c4e3702
cgourlay <colin.gourlay5@gmail.com>
2015-06-30 07:25:25 +0100

    Added bootstrapper and a custom serialiser

---
+++ com.strava.api/Helpers/Nancy/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using Nancy.TinyIoc;
+
+using com.strava.api.Workflows;
+
+namespace com.strava.api.Helpers.Nancy
+{
+    internal sealed class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            container.Register(typeof(ISegmentWorkflow), typeof(SegmentWorkflow));
+        }
+    }
+}
---
+++ com.strava.api/Helpers/Nancy/JsonNetSerializer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Nancy;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace com.strava.api.Helpers
+{
+    public sealed class JsonNetSerializer : ISerializer
+    {
+        private readonly JsonSerializer _serializer;
+
+        public JsonNetSerializer()
+        {
+            _serializer = JsonSerializer.Create(new JsonSerializerSettings());
+            _serializer.Converters.Add(new StringEnumConverter());
+            _serializer.Converters.Add(new BinaryConverter());
+        }
+
+        public bool CanSerialize(string contentType)
+        {
+            return contentType == Representations.BaseRepresentation.ApplicationMediaType;
+        }
+
+        public void Serialize<TModel>(string contentType, TModel model, Stream outputStream)
+        {
+            using (var writer = new JsonTextWriter(new StreamWriter(outputStream)))
+            {
+                _serializer.Serialize(writer, model);
+                writer.Flush();
+            }
+        }
+
+        public IEnumerable<string> Extensions
+        {
+            get { return new List<string>(); }
+        }
+    }
+}
