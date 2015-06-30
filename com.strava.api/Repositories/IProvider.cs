cefa5cde91bea463f17c3c75df14590c03687e05
cgourlay <colin.gourlay5@gmail.com>
2015-06-30 06:45:51 +0100

    Added an IProvider to be used in the Repositories

---
+++ com.strava.api/Repositories/IProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.strava.api.Repositories
+{
+    public interface IProvider<T>
+    {
+        void Create(T businessObject);
+        T Read(Guid businessObjectId);
+        IList<T> Read();
+        void Update(Guid businessObjectId, T updatedBusinessObject);
+        void Delete(Guid businessObjectId);
+    }
+}
