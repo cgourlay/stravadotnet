95edb3a5ef10f40780b218d74c28f363e68c36bc
cgourlay <colin.gourlay5@gmail.com>
2015-07-12 23:23:15 +0100

    Refactor of the SegmentModule tests.

--- com.strava.api/Modules.Tests/SegmentModuleTests.cs
+++ com.strava.api/Modules.Tests/SegmentModuleTests.cs
@@ -6,8 +6,8 @@ using Nancy.Testing;
 using NUnit.Framework;
 
 using SwimBikeRun.Strive.Model.Interfaces.Segments;
-using SwimBikeRun.Strive.Representations;
 using SwimBikeRun.Strive.Representations.Enums;
+using SwimBikeRun.Strive.Representations.Interfaces;
 using SwimBikeRun.Strive.Workflows.Interfaces;
 
 namespace SwimBikeRun.Strive.Modules.Tests
@@ -18,41 +18,19 @@ namespace SwimBikeRun.Strive.Modules.Tests
         public class GetSegment : SegmentModuleTests
         {
             [Test]
-            public void OnlyReturnsNotFoundStatusCodeWhenInvalidIdProvided()
+            public void HandlesApplicationFailure()
             {
-                var handlerMock = new Mock<ISegmentWorkflow>();
-                var browser = new Browser(with =>
-                {
-                    with.Module<SegmentModule>();
-                    with.Dependency<ISegmentWorkflow>(handlerMock.Object);
-                });
-
-                var response = browser.Get("/Segments/qwerty", with =>
-                {
-                    with.HttpRequest();
-                    with.Header("Accept", "application/json");
-                });
-
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-                Assert.That(() => response.Headers["ETag"], Throws.Exception.TypeOf<KeyNotFoundException>());
-                Assert.That(() => response.Headers["Location"], Throws.Exception.TypeOf<KeyNotFoundException>());
-                Assert.That(response.Body, Is.Empty);
-            }
-
-            [Test]
-            public void OnlyReturnsStatusCodeWhenOperationFails()
-            {
-                var handlerMock = new Mock<ISegmentWorkflow>();
-                var responseMock = new Mock<OperationResponse<ISegment>>();
+                var workflowMock = new Mock<ISegmentWorkflow>();
+                var responseMock = new Mock<IOperationResponse<ISegment>>();
 
                 responseMock.SetupProperty(s => s.Status, OperationStatus.InternalServerError);
                 responseMock.SetupProperty(s => s.OperationSucceeded, false);
-                handlerMock.Setup(m => m.GetById(It.IsAny<int>())).Returns(responseMock.Object);
+                workflowMock.Setup(m => m.GetById(It.IsAny<int>())).Returns(responseMock.Object);
 
                 var browser = new Browser(with =>
                 {
                     with.Module<SegmentModule>();
-                    with.Dependency<ISegmentWorkflow>(handlerMock.Object);
+                    with.Dependency(workflowMock.Object);
                 });
 
                 var response = browser.Get("/Segments/1234", with =>
@@ -68,26 +46,76 @@ namespace SwimBikeRun.Strive.Modules.Tests
             }
 
             [Test]
+            public void HandlesInvalidId()
+            {
+                var browser = new Browser(with =>
+                {
+                    with.Module<SegmentModule>();
+                    with.Dependency(new Mock<ISegmentWorkflow>().Object);
+                });
+
+                var response = browser.Get("/Segments/qwerty", with =>
+                {
+                    with.HttpRequest();
+                    with.Header("Accept", "application/json");
+                });
+
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+                Assert.That(() => response.Headers["ETag"], Throws.Exception.TypeOf<KeyNotFoundException>());
+                Assert.That(() => response.Headers["Location"], Throws.Exception.TypeOf<KeyNotFoundException>());
+                Assert.That(response.Body, Is.Empty);
+            }
+        }
+
+        public class ContentNegotiation : SegmentModuleTests
+        {
+            [Test]
             [Ignore("TODO: CG to complete... Refer to issue #31 for further information.")]
-            public void ReturnsEtagHeaderWhenSegmentFound()
+            public void ReturnsEtag()
             {
             }
 
             [Test]
-            public void ReturnsLocationHeaderWhenSegmentFound()
+            public void ReturnsLocation()
             {
-                var handlerMock = new Mock<ISegmentWorkflow>();
-                var responseMock = new Mock<OperationResponse<ISegment>>();
+                var workflowMock = new Mock<ISegmentWorkflow>();
+                var responseMock = new Mock<IOperationResponse<ISegment>>();
 
                 responseMock.SetupProperty(s => s.OperationSucceeded, true);
                 responseMock.SetupProperty(s => s.Status, OperationStatus.Ok);
                 responseMock.SetupProperty(s => s.Data.Id, 229781);
-                handlerMock.Setup(m => m.GetById(229781)).Returns(responseMock.Object);
+                workflowMock.Setup(m => m.GetById(229781)).Returns(responseMock.Object);
 
                 var browser = new Browser(with =>
                 {
                     with.Module<SegmentModule>();
-                    with.Dependency<ISegmentWorkflow>(handlerMock.Object);
+                    with.Dependency(workflowMock.Object);
+                });
+
+                var response = browser.Get("/Segments/229781", with =>
+                {
+                    with.HttpRequest();
+                    with.Header("Accept", "application/json");
+                });
+
+                Assert.That(response.Headers["Location"], Is.EqualTo(string.Format(@"/Segments/{0}", 229781)));
+            }
+
+            [Test]
+            public void ReturnsStatusCode()
+            {
+                var workflowMock = new Mock<ISegmentWorkflow>();
+                var responseMock = new Mock<IOperationResponse<ISegment>>();
+
+                responseMock.SetupProperty(s => s.OperationSucceeded, true);
+                responseMock.SetupProperty(s => s.Status, OperationStatus.Ok);
+                responseMock.SetupProperty(s => s.Data.Id, 229781);
+                workflowMock.Setup(m => m.GetById(229781)).Returns(responseMock.Object);
+
+                var browser = new Browser(with =>
+                {
+                    with.Module<SegmentModule>();
+                    with.Dependency(workflowMock.Object);
                 });
 
                 var response = browser.Get("/Segments/229781", with =>
@@ -97,26 +125,25 @@ namespace SwimBikeRun.Strive.Modules.Tests
                 });
 
                 Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-                Assert.That(response.Headers["Location"], Is.EqualTo(string.Format(@"/Segments/{0}",229781)));
             }
 
             [Test]
-            public void ReturnsModelWhenSegmentFound()
+            public void ReturnsModelAsJson()
             {
-                var handlerMock = new Mock<ISegmentWorkflow>();
-                var responseMock = new Mock<OperationResponse<ISegment>>();
+                var workflowMock = new Mock<ISegmentWorkflow>();
+                var responseMock = new Mock<IOperationResponse<ISegment>>();
 
                 responseMock.SetupProperty(s => s.Status, OperationStatus.Ok);
                 responseMock.SetupProperty(s => s.OperationSucceeded, true);
                 responseMock.SetupProperty(s => s.Data.Id, 229781);
                 responseMock.SetupProperty(s => s.Data.Name, "Hawk Hill");
                 responseMock.Setup(s => s.DataAsJson()).Returns("{ \"name\": \"Hawk Hill\" }");
-                handlerMock.Setup(m => m.GetById(229781)).Returns(responseMock.Object);
-                
+                workflowMock.Setup(m => m.GetById(229781)).Returns(responseMock.Object);
+
                 var browser = new Browser(with =>
                 {
                     with.Module<SegmentModule>();
-                    with.Dependency<ISegmentWorkflow>(handlerMock.Object);
+                    with.Dependency(workflowMock.Object);
                 });
 
                 var response = browser.Get("/Segments/229781", with =>
@@ -126,32 +153,6 @@ namespace SwimBikeRun.Strive.Modules.Tests
                 });
 
                 Assert.That(response.Body.AsString(), Is.StringContaining("Hawk Hill"));
-            }
-
-            [Test]
-            public void ReturnsStatusCodeWhenSegmentFound()
-            {
-                var handlerMock = new Mock<ISegmentWorkflow>();
-                var responseMock = new Mock<OperationResponse<ISegment>>();
-
-                responseMock.SetupProperty(s => s.OperationSucceeded, true);
-                responseMock.SetupProperty(s => s.Status, OperationStatus.Ok);
-                responseMock.SetupProperty(s => s.Data.Id, 229781);
-                handlerMock.Setup(m => m.GetById(229781)).Returns(responseMock.Object);
-
-                var browser = new Browser(with =>
-                {
-                    with.Module<SegmentModule>();
-                    with.Dependency<ISegmentWorkflow>(handlerMock.Object);
-                });
-
-                var response = browser.Get("/Segments/229781", with =>
-                {
-                    with.HttpRequest();
-                    with.Header("Accept", "application/json");
-                });
-
-                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             }
         }
     }
