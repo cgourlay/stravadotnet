210030c2da6b0abe7b1faf4bdf17fae914f93ee6
cgourlay <colin.gourlay5@gmail.com>
2015-07-14 23:24:12 +0100

    Added Endpoints to the Representations assembly.

---
+++ SwimBikeRun.Strive.Representations/Endpoints.cs
@@ -0,0 +1,20 @@
+﻿namespace SwimBikeRun.Strive.Representations
+{
+    public class Endpoints
+    {
+        public readonly string Activity = @"https://www.strava.com/api/v3/activities";
+        public readonly string Activities = @"https://www.strava.com/api/v3/athlete/activities";
+        public readonly string ActivitiesFollowers = @"https://www.strava.com/api/v3/activities/following";
+        public readonly string Athlete = @"https://www.strava.com/api/v3/athlete";
+        public readonly string Athletes = @"https://www.strava.com/api/v3/athletes";
+        public readonly string Club = @"https://www.strava.com/api/v3/clubs";
+        public readonly string Clubs = @"https://www.strava.com/api/v3/athlete/clubs";
+        public readonly string Friends = @"https://www.strava.com/api/v3/athlete/friends";
+        public readonly string Follower = @"https://www.strava.com/api/v3/athlete/followers";
+        public readonly string Followers = @"https://www.strava.com/api/v3/athletes";
+        public readonly string Gear = @"https://www.strava.com/api/v3/gear";
+        public readonly string Leaderboard = @"https://www.strava.com/api/v3/segments";
+        public readonly string Starred = @"https://www.strava.com/api/v3/segments/starred";
+        public readonly string Uploads = @"https://www.strava.com/api/v3/uploads/";
+    }
+}
