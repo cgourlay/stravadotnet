fe78859e04e75660e26ab08a934b839e084deef1
cgourlay <colin.gourlay5@gmail.com>
2015-09-15 13:35:05 +0100

    Send visual cues when running the service.

--- SwimBikeRun.Strive.Modules/Program.cs
+++ SwimBikeRun.Strive.Modules/Program.cs
@@ -7,11 +7,15 @@ namespace SwimBikeRun.Strive.Modules
 {
     public static class Program
     {
+        private static readonly Uri Endpoint = new Uri(ConfigurationManager.AppSettings["HostUri"]);
+
         static void Main()
         {   
-            using (var host = new NancyHost(new Uri(ConfigurationManager.AppSettings["HostUri"])))
+            using (var host = new NancyHost(Endpoint))
             {
                 host.Start();
+                Console.WriteLine("Service running on: {0}", Endpoint.AbsoluteUri);
+                Console.WriteLine("Press any key to quit.");
                 Console.ReadLine();
             }
         }
